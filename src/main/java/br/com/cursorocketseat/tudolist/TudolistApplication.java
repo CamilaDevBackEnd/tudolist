b2f6a45adcd33e3d5c3dda4d51d45ea9468c8d30
CamilaDevBackEnd <camila_guimaraes18@hotmail.com>
2023-10-12 16:13:52 -0300

    first commit

---
+++ src/main/java/br/com/cursorocketseat/tudolist/TudolistApplication.java
@@ -0,0 +1,19 @@
+package br.com.cursorocketseat.tudolist;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.cursorocketseat.tudolist.TudolistApplication;
+
+@SpringBootApplication
+
+public class TudolistApplication {
+
+    public static void main(String[] args) {
+		SpringApplication.run(TudolistApplication.class, args);
+	}
+
+
+    
+}
---
+++ src/test/java/br/com/cursorocketseat/tudolist/TudolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cursorocketseat.tudolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TudolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
